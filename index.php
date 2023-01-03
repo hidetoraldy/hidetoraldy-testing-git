a3ede954726cebd8f25f220f80d323fcf48f7192
Aldy Ramadhan <hidetoraldy@gmail.com>
2023-01-03 14:48:27 +0700

    menambahkan list

--- index.php
+++ index.php
@@ -9,5 +9,9 @@
 <body>
     <h2>Hello World!</h2>
     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Suscipit voluptate, non placeat, consequatur totam incidunt, dolore blanditiis architecto temporibus aut nemo est repellat provident. Natus dolores est tempora similique consectetur?</p>
+    <ul>
+        <li>satu</li>
+        <li>dua</li>
+    </ul>
 </body>
 </html>
